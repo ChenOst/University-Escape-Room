41ad9390e8d25a3cd68de977c379a02ba3d3d8c0
Chen Ostrovski <chenostrovski@gmail.com>
2020-05-10 21:31:17 +0300

    Scences added

---
+++ University Escape Room/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Starting new game: " + System.DateTime.Now);
+        SceneManager.LoadScene("GameScene");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Exit the game: " + System.DateTime.Now);
+        Application.Quit();
+    }
+}
