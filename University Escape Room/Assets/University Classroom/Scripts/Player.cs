e105f7c326bbfe51bf2b20717db18a69bb97099c
Chen Ostrovski <chenostrovski@gmail.com>
2020-05-14 23:54:41 +0300

    Switch lights

---
+++ University Escape Room/Assets/University Classroom/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticallInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticallInput, 0);
+
+        transform.Translate(direction * 5 * Time.deltaTime);
+    }
+}
---
+++ University Escape Room/Assets/University Classroom/Scripts/TurnTheLights.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TurnTheLights : MonoBehaviour
+{
+    private bool lightsOn = false;
+    private bool canActive = false;
+    private GameObject[] _lights;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lights = GameObject.FindGameObjectsWithTag("Light");
+        if (_lights != null)
+        {
+            foreach (GameObject light in _lights)
+            {
+                light.SetActive(lightsOn);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canActive = true;
+            panel.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canActive = false;
+            panel.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if (canActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("lightsOn " + lightsOn.ToString());
+
+                lightsOn = !lightsOn;
+                foreach (GameObject light in _lights)
+                {
+                    light.SetActive(lightsOn);
+                }
+            }
+        }
+    }
+}
