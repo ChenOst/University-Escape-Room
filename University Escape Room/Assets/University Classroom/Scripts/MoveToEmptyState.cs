2f22477e3f3a90410db5ea20c6fd92baf4924056
Chen Ostrovski <chenostrovski@gmail.com>
2020-05-29 20:03:54 +0300

    Wake up effect added to game

---
+++ University Escape Room/Assets/University Classroom/Scripts/MoveToEmptyState.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class MoveToEmptyState : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            animator.SetTrigger("MoveToEmpty");
+            animator.enabled = false;
+        }
+    }
+
+}
